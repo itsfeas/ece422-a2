86175b5a8a0b36a993eab4b021fce1349d1e0a95
itsfeas <amf.fiaz@gmail.com>
2024-03-01 02:00:47 -0700

    complete draft of  models

--- model/src/model.rs
+++ model/src/model.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+
+#[derive()]
+struct FNode {
+    id: String,
+    name: String,
+    path: String,
+    owner: String,
+    hash: i128,
+    parent: String,
+    has_children: bool,
+    u: i8,
+    g: i8,
+    o: i8,
+    
+    //if directory
+    children: Vec<String>,
+}
+
+#[derive()]
+struct UserGroupNode {
+    id: String,
+    name: String,
+    has_children: bool,
+
+    //if group
+    users: Vec<String>,
+
+    //if user
+    is_admin: bool,
+    salt: String,
+    user_group: String,
 }
