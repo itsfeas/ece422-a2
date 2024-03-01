ff4df708e8247ae5ac4ffe559cf5aa9a9053cb60
Zony Yu <zony249@gmail.com>
2024-03-01 14:03:59 -0700

    simple client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,17 @@
+use std::net::{TcpListener, TcpStream};
+use std::string::String; 
+use std::io::{Write, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let server_addr = "127.0.0.1:2222";
+    let mut stream = TcpStream::connect(server_addr).expect("TCP connection failed"); 
+
+    while true {
+        let message = String::from("test message");
+        let mut buffer = Vec::new(); 
+        stream.write(&message.as_bytes()).expect("Error writing");
+        stream.read(&mut buffer).expect("Error reading"); 
+
+        println!("{}", String::from_utf8(buffer.clone()).unwrap()); 
+    }
 }
