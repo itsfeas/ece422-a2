0bf42e3a7bb2497c9a2b7086498311bade9dd7fd
itsfeas <amf.fiaz@gmail.com>
2024-03-01 02:13:26 -0700

    convert to use bigserial

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,23 @@
 CREATE TABLE fnode (
-    id VARCHAR PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR,
     path VARCHAR,
-    owner VARCHAR,
+    owner BIGSERIAL FOREIGN KEY user_group_node.id,
     hash NUMERIC,
-    parent VARCHAR,
+    parent BIGSERIAL,
     has_children BOOLEAN,
     u SMALLINT,
     g SMALLINT,
     o SMALLINT,
-    children TEXT[]
+    children BIGSERIAL[]
 );
 
 CREATE TABLE user_group_node (
-    id VARCHAR PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR,
     has_children BOOLEAN,
-    users TEXT[],
+    users BIGSERIAL[],
     is_admin BOOLEAN,
     salt VARCHAR,
-    user_group VARCHAR
+    user_group BIGSERIAL
 );
