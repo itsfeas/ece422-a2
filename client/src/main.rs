2436560a733d694186fe754d80e63c8d2d2ee422
Zony Yu <zony249@gmail.com>
2024-03-04 11:48:11 -0700

    first commit for deffie-hellman

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,11 +1,13 @@
 use std::error::Error; 
 use tungstenite::{connect, Message}; 
+use x25519_dalek::{EphemeralSecret, PublicKey};
+use url::Url; 
 
 
 fn main() -> Result<(), Box<dyn Error>> {
     let server_addr = "127.0.0.1:8080";
     
-    let url = url::Url::parse("ws://127.0.0.1:8080").expect("Failed to unwrap addr"); 
+    let url = Url::parse("ws://127.0.0.1:8080").expect("Failed to unwrap addr"); 
     let (mut socket, response) = connect(url).expect("Failed to connect");
     println!("WebSocket handshake has been successfully completed");
 
@@ -16,4 +18,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+
+fn key_exchange(server_addr: &Url) {
+    
+    let rand_num = rand::thread_rng(); 
+    let client_secret = EphemeralSecret::new(rand_num);
+    let client_public = PublicKey::from(&client_secret);
+
+    // make call to server for server'publicKey 
+    //
+    
+    let (mut socket, response) = connect(server_addr.clone()).expect("Failed to connect");
+
+    socket.send(Message::Text("new".into())).unwrap();
+    let msg = socket.read().expect("Error reading message");
+    println!("Received: {}", msg);
+
+    // deffie-hellmamn
+
 }
