fb51f1faf684a0e056ff343c11796da162d9d907
Zony Yu <zony249@gmail.com>
2024-03-04 11:01:24 -0700

    synchronous client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,39 +1,19 @@
-use futures::TryStreamExt;
-use tokio::net::{TcpListener, TcpStream};
-use std::string::String; 
-// use std::io::{Write, Read};
-use tokio::io::{AsyncWriteExt, AsyncReadExt}; 
 use std::error::Error; 
-use tokio_tungstenite::{connect_async, client_async_tls, tungstenite::Message};
-use futures_util::StreamExt;
-use futures_util::SinkExt;
+use tungstenite::{connect, Message}; 
 
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let server_addr = "127.0.0.1:8080";
-
-    let url = url::Url::parse("ws://127.0.0.1:8080").unwrap();
-    println!("URL: {}", url);
-    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+    
+    let url = url::Url::parse("ws://127.0.0.1:8080").expect("Failed to unwrap addr"); 
+    let (mut socket, response) = connect(url).expect("Failed to connect");
     println!("WebSocket handshake has been successfully completed");
 
-    let (mut w, mut r) = ws_stream.split();
-    
     loop {
-        let message = String::from("test message");
-        w.send(Message::Text(message.clone())).await;
-        println!("Message sent :{:?}", &message);
-            
-        while let Some(val) = r.next().await {
-            let value = val?; 
-            if value.is_text() || value.is_binary() {
-                println!("Received: {:?}", value); 
-            }
-        }
-
-    };
-
+        socket.send(Message::Text("Hello WebSocket".into())).unwrap();
+        let msg = socket.read().expect("Error reading message");
+        println!("Received: {}", msg);
+    }
 
     Ok(())
 }
