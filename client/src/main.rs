eeaf5d995ef949856b2788fd70a209fa1dbaf84c
Zony Yu <zony249@gmail.com>
2024-03-04 19:46:09 -0700

    client now simulates user typing in the new command

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,14 +1,29 @@
-use std::error::Error; 
-use tungstenite::{connect, Message}; 
+use std::io::Error; 
+use tungstenite::{connect, Message, WebSocket}; 
 use x25519_dalek::{EphemeralSecret, PublicKey, SharedSecret};
 use url::Url; 
-use serde::{Serialize, Deserialize}; 
+use serde::{Serialize, Deserialize};
+use futures::Stream;
 
 use model::model::AppMessage;  
 // use serde::{serialize, deserialize}; 
 
 
-fn main() -> Result<(), Box<dyn Error>> {
+enum LoginStatus {
+    New(String),
+    Attempt(String), 
+    Failed(String)
+}
+
+
+
+/////////////////////////////////
+///                           ///
+/// FUNCTIONS                 ///
+///                           ///
+/////////////////////////////////
+
+fn main() -> Result<(), Error> {
     let server_addr = "127.0.0.1:8080";
     
     let url = Url::parse("ws://127.0.0.1:8080").expect("Failed to unwrap addr"); 
@@ -16,16 +31,58 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("WebSocket handshake has been successfully completed");
 
     loop {
-        socket.send(Message::Text("Hello WebSocket".into())).unwrap();
-        let msg = socket.read().expect("Error reading message");
-        println!("Received: {}", msg);
+
+        // obtain input from command line 
+        let cmd_input = String::from("new"); 
+        
+        try_login(cmd_input, &mut socket); 
+
+
     }
 
     Ok(())
 }
 
 
-fn key_exchange(server_addr: &Url) -> SharedSecret {
+fn try_login<S>(input_str: String, socket: &mut WebSocket<S>) -> LoginStatus where S: std::io::Read, S: std::io::Write  {
+    let app_message = command_parser(input_str).unwrap();  
+    println!("DEBUG: {:?}", app_message); 
+    
+    if app_message.cmd == "new" {
+        println!("DEBUG: reached"); 
+        let shared_secret = key_exchange(socket); 
+        println!("DEBUG: {:?}", Vec::from(shared_secret.as_ref()));  
+        // user types username 
+        let username = String::from("itsnotfeas"); 
+
+        // encrypt 
+        
+        // send to socket (username, encrypted username)
+
+        return LoginStatus::New(username); 
+    }
+
+    LoginStatus::Failed("".to_string())
+    // else
+}
+
+fn command_parser(input_str: String) -> Result<AppMessage, String> {
+    let mut result = input_str.split_whitespace().map(|simple_str| String::from(simple_str)).collect::<Vec<String>>();  
+
+    let message = match result.pop() {
+        Some(cmd_val) => {
+            AppMessage {
+                cmd: cmd_val, 
+                data: result 
+            }
+        }, 
+        None => return Err("Failure".to_string()), 
+    }; 
+    return Ok(message); 
+}
+
+
+fn key_exchange<S>(socket: &mut WebSocket<S>) -> SharedSecret where S: std::io::Read, S: std::io::Write { 
     
     let rand_num = rand::thread_rng(); 
     let client_secret = EphemeralSecret::new(rand_num);
@@ -34,14 +91,14 @@ fn key_exchange(server_addr: &Url) -> SharedSecret {
     // make call to server for server'publicKey 
     //
     
-    let (mut socket, response) = connect(server_addr.clone()).expect("Failed to connect");
     let app_message = AppMessage {
         cmd: String::from("new"), 
-        data: vec![String::from("")]
+        data: vec![serde_json::to_string(&client_public).unwrap()]
     }; 
 
 
     socket.send(Message::Text(serde_json::to_string(&app_message).unwrap())).unwrap();
+    println!("Message sent"); 
     let msg = socket.read().expect("Error reading message");
     println!("Received: {}", msg);
 
