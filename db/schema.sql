72af0ae178fe40bf79274b6b5b75974481f4f4b1
itsfeas <amf.fiaz@gmail.com>
2024-03-30 14:42:32 -0600

    fix db schema bug

--- db/schema.sql
+++ db/schema.sql
@@ -12,17 +12,17 @@ CREATE TABLE fnode (
     children BIGINT[]
 );
 
-CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    user_name VARCHAR,
-    group BIGINT FOREIGN KEY groups.id,
-    key VARCHAR,
-    salt VARCHAR,
-    is_admin BOOLEAN
-);
-
 CREATE TABLE groups (
     id BIGSERIAL PRIMARY KEY,
     users BIGINT[],
     name VARCHAR
+);
+
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    user_name VARCHAR,
+    group_id BIGINT REFERENCES groups(id),
+    key VARCHAR,
+    salt VARCHAR,
+    is_admin BOOLEAN
 );
