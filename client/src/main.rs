5b2eed8243de25665672e18d6f24254a68e3464f
Zony Yu <zony249@gmail.com>
2024-03-30 22:16:01 -0600

    fixed mkdir again

--- client/src/main.rs
+++ client/src/main.rs
@@ -344,13 +344,12 @@ fn mkdir<S>(msg: AppMessage,
     if recv_msg.cmd == Cmd::Mkdir {
         // let enc_filename = get_encrypted_filenames(vec![target_path.clone()], socket, encryption_key).unwrap()[0].clone(); 
         let enc_filename = recv_msg.data[0].clone(); 
-        let mut enc_path = convert_path_to_enc(&enc_filename, current_path, socket, encryption_key); 
+        let mut enc_path = convert_path_to_enc(&target_path, current_path, socket, encryption_key); 
         if enc_path.path.len() == 0 { // cd to root and home level
             println!("{}", "Cannot mkdir on this level.") 
         } else {
             // for cd, do nothing
             //
-            enc_path.path.push((true, enc_filename)); 
             create_dir(enc_path.path.iter().map(|x| {
                     if !x.0 {
                         panic!("Path must be encrypted"); 
@@ -575,6 +574,7 @@ fn preprocess_app_message(app_msg: &mut AppMessage, current_path: &Path) -> Resu
 }
 
 /*  ONLY FOR CLIENT SIDE USE
+ *  final encrypted path will include the filename
  *  
  * */
 fn convert_path_to_enc<S>(filename: &String, 
